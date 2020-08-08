d112a2789d94bcc5ffddba4f579e7cbeb04be5cc
devpolatto <polatto.xcode.job@gmail.com>
2020-08-07 21:48:04 -0300

    changing the folder path

---
+++ src/pages/Landing/TeacherFomr/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const TeacherFomr: React.FC = () => {
+  return (
+      <h1>teacher form</h1>
+  );
+}
+
+export default TeacherFomr;
---
+++ src/pages/Landing/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TeacherList: React.FC = () => {
+  return (
+      <h1>teahcer</h1>
+  );
+}
+
+export default TeacherList;
