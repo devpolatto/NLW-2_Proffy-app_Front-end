43dc9042573bd69ceb2438529a8e2952d2f801d3
devpolatto <polatto.xcode.job@gmail.com>
2020-08-07 22:26:15 -0300

    finalized the structure and css of the Teacher List component

--- src/pages/Landing/TeacherList/index.tsx
+++ src/pages/Landing/TeacherList/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import logoImg  from '../../../assets/images/logo.svg';
+import backIcon  from '../../../assets/images/icons/back.svg';
+
+import './styles.css';
+
 
 const TeacherList: React.FC = () => {
   return (
-      <h1>teahcer</h1>
+      <div id="page-teacher-list" className="container">
+        <header className="page-header">
+
+          <div className="top-bar-container">
+            <Link to="/">
+              <img src={backIcon} alt="voltar"/>
+            </Link>
+            <img src={logoImg} alt="proffy"/>
+          </div>
+
+          <div className="header-content">
+            <strong>eeeeeeeeeeeeeeeeeeeeeeeee</strong>
+          </div>
+
+        </header>
+      </div>
   );
 }
 
